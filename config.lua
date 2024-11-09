e70eed1ef86fb0a9e10aa5a213702fd4812a3983
Lucas Teles <lucasteles22@gmail.com>
2024-11-09 20:13:53 -0300

    update config

--- config.lua
+++ config.lua
@@ -1,48 +1,52 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
 
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+-- lvim.colorscheme = "onedarker"
+lvim.colorscheme = "hybrid_material"
+lvim.transparent_window = true
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
-
-
 -- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Live Grep" }
+-- lvim.builtin.which_key.mappings["F"] = { "<cmd>Telescope find_files theme=ivy<cr>", "Find Files" }
+lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Live Grep" }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+    name = "+Trouble",
+    r = { "<cmd>Trouble lsp_references<cr>", "References" },
+    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+    d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+    w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
 }
-
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.builtin.dashboard.active = true
-lvim.builtin.notify.active = true
+lvim.builtin.lualine.style = "default" -- or "none"
+-- lvim.builtin.dashboard.active = true
+-- lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+-- lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+    "bash",
+    "c",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "java",
+    "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -75,14 +79,14 @@ lvim.builtin.treesitter.highlight.enabled = true
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
     -- { command = "uncrustify", filetypes = { "java"} },
-  -- { command = "black", filetypes = { "python" } },
-  -- { command = "isort", filetypes = { "python" } },
-  -- {
-  --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --   command = "prettier",
-  --   ---@usage arguments to pass to the formatter
-  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --   extra_args = { "--print-with", "100" },
+    -- { command = "black", filetypes = { "python" } },
+    -- { command = "isort", filetypes = { "python" } },
+    -- {
+    --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    --   command = "prettier",
+    --   ---@usage arguments to pass to the formatter
+    --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    --   extra_args = { "--print-with", "100" },
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
     -- filetypes = { "typescript", "typescriptreact" }
 }
@@ -107,66 +111,110 @@ formatters.setup {
 
 -- Additional Plugins
 lvim.plugins = {
-    {"mfussenegger/nvim-jdtls", ft = "java"},
-    {"folke/tokyonight.nvim"},
-    {"phaazon/hop.nvim"}, -- optional but strongly recommende
+    { "mfussenegger/nvim-jdtls", ft = "java" },
+    { "folke/tokyonight.nvim" },
+    { "phaazon/hop.nvim" }, -- optional but strongly recommende
     {
-      "folke/trouble.nvim",
-      cmd = "TroubleToggle",
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+    },
+    {
+        "folke/lsp-colors.nvim",
+        event = "BufRead",
+    },
+    -- {
+    --     "vim-airline/vim-airline",
+    -- },
+    -- {
+    --     "vim-airline/vim-airline-themes",
+    -- },
+    {
+        "terryma/vim-multiple-cursors",
+    },
+    {
+        "mrjones2014/nvim-ts-rainbow",
+    },
+    {
+        "kristijanhusak/vim-hybrid-material",
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = {
+            "G",
+            "Git",
+            "Gdiffsplit",
+            "Gread",
+            "Gwrite",
+            "Ggrep",
+            "GMove",
+            "GDelete",
+            "GBrowse",
+            "GRemove",
+            "GRename",
+            "Glgrep",
+            "Gedit"
+        },
+        ft = { "fugitive" }
     },
 }
-require'hop'.setup()
+require 'hop'.setup()
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
 
 local options = {
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 250,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+    backup = false,                          -- creates a backup file
+    clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+    cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0,                        -- so that `` is visible in markdown files
+    fileencoding = "utf-8",                  -- the encoding written to a file
+    hlsearch = true,                         -- highlight all matches on previous search pattern
+    ignorecase = true,                       -- ignore case in search patterns
+    mouse = "a",                             -- allow the mouse to be used in neovim
+    pumheight = 10,                          -- pop up menu height
+    showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+    showtabline = 2,                         -- always show tabs
+    smartcase = true,                        -- smart case
+    smartindent = true,                      -- make indenting smarter again
+    splitbelow = true,                       -- force all horizontal splits to go below current window
+    splitright = true,                       -- force all vertical splits to go to the right of current window
+    swapfile = false,                        -- creates a swapfile
+    termguicolors = true,                    -- set term gui colors (most terminals support this)
+    timeoutlen = 250,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = true,                         -- enable persistent undo
+    updatetime = 300,                        -- faster completion (4000ms default)
+    writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    expandtab = true,                        -- convert tabs to spaces
+    shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+    tabstop = 2,                             -- insert 2 spaces for a tab
+    cursorline = true,                       -- highlight the current line
+    number = true,                           -- set numbered lines
+    relativenumber = true,                   -- set relative numbered lines
+    numberwidth = 4,                         -- set number column width to 2 {default 4}
+    signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+    wrap = false,                            -- display lines as one long line
+    scrolloff = 8,                           -- is one of my fav
+    sidescrolloff = 8,
+    guifont = "monospace:h17",               -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append "c"
+vim.g.enable_bold_font = 1
+
+-- vim.g.airline_powerline_fonts = 1
+-- vim.g.airline_theme = 'simple'
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 -- add your own keymapping
 lvim.keys.normal_mode["<leader><leader>s"] = ":HopPattern<CR>"
+lvim.keys.normal_mode["<C-p>"] = ":Telescope find_files<CR>"
 lvim.keys.visual_block_mode["J"] = false
 lvim.keys.visual_block_mode["K"] = false
 lvim.keys.normal_mode["gt"] = ":bnext<CR>"
 lvim.keys.normal_mode["gT"] = ":bprevious<CR>"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
